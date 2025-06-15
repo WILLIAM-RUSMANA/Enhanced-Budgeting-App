51301300f484a6713f7a3817ff78467bcabd0ede
WILLIAM-RUSMANA <93782439+WILLIAM-RUSMANA@users.noreply.github.com>
2025-06-15 23:38:57 +0700

    fixed return type for budgetsort and expesnesort

--- OOP/BudgetSort.java
+++ OOP/BudgetSort.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class BudgetSort {
-    public static ArrayList<Budget> sort(ArrayList<Budget> budgets) {
+    // Sorts out all elements in the array from earliest to latest
+    public static void sort(ArrayList<Budget> budgets) {
         budgets.sort(Comparator
                 .comparingInt(Budget::getYear)
                 .thenComparingInt(Budget::getMonth));
-
-        return budgets;
     }
 }
--- OOP/ExpenseSort.java
+++ OOP/ExpenseSort.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class ExpenseSort {
+    // Sorts out all elements in the array from earliest to latest
     public static void sort(ArrayList<Expense> expenses) {
         expenses.sort(Comparator
                 .comparingInt(Expense::getYear)
